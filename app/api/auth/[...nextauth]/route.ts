ffbad279b2475a03e54923a5c374955a6abf12df
CMMS Developer <dev@cmms.local>
2026-01-25 10:05:20 +0100

    Fix file encoding (Manual Batch)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
 import { handlers } from "@/auth"; // Referring to the auth.ts we created
 export const { GET, POST } = handlers;
-export const runtime = "nodejs"; 
+export const dynamic = "force-dynamic";
