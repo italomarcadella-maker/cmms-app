c88fe6bd8013496083fddcedf95656f4fddf62c8
CMMS Developer <dev@cmms.local>
2026-01-25 10:36:40 +0100

    Fix file encoding (Manual Batch)

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
 
-export default NextAuth(authConfig).auth;
+// export default NextAuth(authConfig).auth;
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    return NextResponse.next();
+}
 
 export const config = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
